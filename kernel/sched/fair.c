35b384a0c7f88a47ca63791ddcb015ab6bc46d9c
Ionela Voinescu <ionela.voinescu@arm.com>
2018-04-27 18:19:13 +0100

    ANDROID: sched/fair: unify spare capacity calculation
    Given that we have a few sites where the spare capacity of a CPU is
    calculated as the difference between the original capacity of the CPU
    and its computed new utilization, let's unify the calculation and use
    that value tracked with a local spare_cap variable.
    
    Change-Id: I78daece7543f78d4f74edbee5e9ceb62908af507
    Signed-off-by: Ionela Voinescu <ionela.voinescu@arm.com>
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7063,7 +7063,7 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 	int best_idle_cpu = -1;
 	int target_cpu = -1;
 	int cpu, i;
-	long spare_cap, most_spare_cap = 0;
+	long spare_wake_cap, most_spare_wake_cap = 0;
 	int most_spare_cap_cpu = -1;
 	unsigned int active_cpus_count = 0;
 	int isolated_candidate = -1;
@@ -7108,6 +7108,7 @@ retry:
 			unsigned long capacity_curr = capacity_curr_of(i);
 			unsigned long capacity_orig = capacity_orig_of(i);
 			unsigned long wake_util, new_util;
+			long spare_cap;
 
 			cpumask_clear_cpu(i, &search_cpus);
 
@@ -7130,10 +7131,10 @@ retry:
 			 */
 			wake_util = cpu_util_wake(i, p);
 			new_util = wake_util + task_util(p);
-			spare_cap = capacity_orig_of(i) - wake_util;
+			spare_wake_cap = capacity_orig_of(i) - wake_util;
 
-			if (spare_cap > most_spare_cap) {
-				most_spare_cap = spare_cap;
+			if (spare_wake_cap > most_spare_wake_cap) {
+				most_spare_wake_cap = spare_wake_cap;
 				most_spare_cap_cpu = i;
 			}
 
@@ -7146,6 +7147,13 @@ retry:
 
 			if (cpu_check_overutil_condition(i, new_util))
 				continue;
+
+			/*
+			 * Pre-compute the maximum possible capacity we expect
+			 * to have available on this CPU once the task is
+			 * enqueued here.
+			 */
+			spare_cap = capacity_orig - new_util;
 
 			/*
 			 * Case A) Latency sensitive tasks
@@ -7198,9 +7206,9 @@ retry:
 				 * Case A.2: Target ACTIVE CPU
 				 * Favor CPUs with max spare capacity.
 				 */
-				if ((capacity_curr > new_util) &&
-					(capacity_orig - new_util > target_max_spare_cap)) {
-					target_max_spare_cap = capacity_orig - new_util;
+				if (capacity_curr > new_util &&
+				    spare_cap > target_max_spare_cap) {
+					target_max_spare_cap = spare_cap;
 					target_cpu = i;
 					continue;
 				}
@@ -7312,10 +7320,10 @@ retry:
 
 			/* Favor CPUs with maximum spare capacity */
 			if (capacity_orig >= target_capacity &&
-			    (capacity_orig - new_util) < target_max_spare_cap)
+			    spare_cap < target_max_spare_cap)
 				continue;
 
-			target_max_spare_cap = capacity_orig - new_util;
+			target_max_spare_cap = spare_cap;
 			target_capacity = capacity_orig;
 			target_util = new_util;
 			target_cpu = i;
