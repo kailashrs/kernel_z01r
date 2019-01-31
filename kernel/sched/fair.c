b2131dfd77fbf4d7ef9d9820d02245ef8f69e907
Rick Yiu <rickyiu@google.com>
2019-01-31 15:23:14 +0800

    sched/fair: apply sync wake-up to pure CFS path
    Since CONFIG_SCHED_WALT is disabled, we need another way to boost
    perf as sched_boost does, and skipping EAS has similar effect. We
    use powerhal to handle it. Also apply sync wake-up so that pure
    CFS path (when skipping EAS) can benefit from it.
    
    Bug: 119932121
    Test: boot to home, operation normal, app launch nornal
    Change-Id: I441edea7f9464dc000580c52639af164d46ce5c4
    Signed-off-by: Rick Yiu <rickyiu@google.com>
    
    [kailashrs: adjust about_to_idle]
    Signed-off-by: Kailash <kailash.sudhakar@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7889,9 +7889,23 @@ select_task_rq_fair(struct task_struct *p, int prev_cpu, int sd_flag, int wake_f
 	int sync = (wake_flags & WF_SYNC) && !(current->flags & PF_EXITING);
 
 	if (sd_flag & SD_BALANCE_WAKE) {
+		int _wake_cap = wake_cap(p, cpu, prev_cpu);
+		int _cpus_allowed = cpumask_test_cpu(cpu, tsk_cpus_allowed(p));
+
+		if (_cpus_allowed) {
+			bool about_to_idle = sysctl_sched_sync_hint_enable &&
+				(cpu_rq(cpu)->nr_running < 2) && idle_cpu(cpu);
+
+			if (!_wake_cap && about_to_idle) {
+				rcu_read_unlock();
+				return cpu;
+			}
+		}
+
 		record_wakee(p);
-		want_affine = (!wake_wide(p) && !wake_cap(p, cpu, prev_cpu) &&
-			cpumask_test_cpu(cpu, tsk_cpus_allowed(p)));
+		want_affine = (!wake_wide(p) &&
+			       !_wake_cap &&
+			       _cpus_allowed);
 	}
 
 	if (energy_aware() && !(cpu_rq(prev_cpu)->rd->overutilized)) {
