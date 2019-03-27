9ca9ef23265e27fb8d8f347cd11c27e988fbb7aa
Francisco Franco <franciscofranco.1990@gmail.com>
2019-03-27 19:49:28 +0000

    msm: dsi-display: backlight_min shouldn't be lower than 1
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_display.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_display.c
@@ -32,8 +32,8 @@
 #include "dsi_pwr.h"
 #include "sde_dbg.h"
 
-int backlight_min = 0;
-module_param(backlight_min, int, 0644);
+static unsigned short backlight_min = 1;
+module_param(backlight_min, short, 0644);
 
 #define to_dsi_display(x) container_of(x, struct dsi_display, host)
 #define INT_BASE_10 10
@@ -177,6 +177,9 @@ int dsi_display_set_backlight(void *display, u32 bl_lvl)
 
 	bl_scale_ad = panel->bl_config.bl_scale_ad;
 	bl_temp = (u32)bl_temp * bl_scale_ad / MAX_AD_BL_SCALE_LEVEL;
+
+	if (!backlight_min)
+		backlight_min = 1;
 
 	if (bl_temp != 0 && bl_temp < backlight_min)
 		bl_temp = backlight_min;
