067196e5ced22bb166a7ea5a74a81d732b7d395e
rohan <purohit.rohan@gmail.com>
2019-06-28 11:52:30 +0530

    focaltech_touch: Send KEY_WAKEUP event for dt2w

--- drivers/input/touchscreen/focaltech_touch/focaltech_gesture.c
+++ drivers/input/touchscreen/focaltech_touch/focaltech_gesture.c
@@ -596,7 +596,7 @@ static void fts_gesture_report(struct input_dev *input_dev, int gesture_id)
         gesture = KEY_GESTURE_DOWN;
         break;
     case GESTURE_DOUBLECLICK:
-        gesture = KEY_GESTURE_U;
+        gesture = KEY_WAKEUP;
         break;
     case GESTURE_O:
         gesture = KEY_GESTURE_O;
@@ -637,7 +637,7 @@ static void fts_gesture_report(struct input_dev *input_dev, int gesture_id)
 			return;
 
 		   switch (gesture){
-		   case KEY_GESTURE_U:
+		   case KEY_WAKEUP:
 				if (dclick_report_flag)
 				{
 					input_report_key(input_dev, gesture, 1);
@@ -948,7 +948,7 @@ int fts_gesture_init(struct fts_ts_data *ts_data)
 
     FTS_FUNC_ENTER();
     input_set_capability(input_dev, EV_KEY, KEY_POWER);
-    input_set_capability(input_dev, EV_KEY, KEY_GESTURE_U);
+    input_set_capability(input_dev, EV_KEY, KEY_WAKEUP);
     input_set_capability(input_dev, EV_KEY, KEY_GESTURE_UP);
     input_set_capability(input_dev, EV_KEY, KEY_GESTURE_DOWN);
     input_set_capability(input_dev, EV_KEY, KEY_GESTURE_LEFT);
@@ -967,7 +967,7 @@ int fts_gesture_init(struct fts_ts_data *ts_data)
     __set_bit(KEY_GESTURE_LEFT, input_dev->keybit);
     __set_bit(KEY_GESTURE_UP, input_dev->keybit);
     __set_bit(KEY_GESTURE_DOWN, input_dev->keybit);
-    __set_bit(KEY_GESTURE_U, input_dev->keybit);
+    __set_bit(KEY_WAKEUP, input_dev->keybit);
     __set_bit(KEY_GESTURE_O, input_dev->keybit);
     __set_bit(KEY_GESTURE_E, input_dev->keybit);
     __set_bit(KEY_GESTURE_M, input_dev->keybit);
