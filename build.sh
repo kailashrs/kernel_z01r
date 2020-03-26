40f3cbb25d1ee0ab01d19326c09199f93ef81568
Kailash <kailash.sudhakar@gmail.com>
2020-03-26 19:04:40 +0530

    Add build script

---
+++ build.sh
@@ -0,0 +1,15 @@
+make clean
+export ARCH="arm64"
+export PATH="/usr/lib/ccache/bin:$PATH"
+export CROSS_COMPILE="ccache /home/kailashrs/5z/arm64-gcc/bin/aarch64-elf-"
+export CROSS_COMPILE_ARM32="ccache /home/kailashrs/5z/arm32-gcc/bin/arm-eabi-"
+make O=out Z01R_defconfig
+make O=out -j$(nproc --all)
+cd ..
+cd AnyKernel3
+rm zImage
+rm dtbo.img
+rm glitch3d.zip
+python2 /home/kailashrs/5z/mkdtboimg.py create dtbo.img /home/kailashrs/5z/msm-4.9/out/arch/arm64/boot/dts/qcom/*.dtbo
+cp /home/kailashrs/5z/msm-4.9/out/arch/arm64/boot/Image.gz-dtb zImage
+zip -r glitch3d.zip *
