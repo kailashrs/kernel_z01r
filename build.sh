5796b88f9baab4f6a7479f4d706b1c9335622643
Kailash <kailash.sudhakar@gmail.com>
2020-04-25 14:35:31 +0530

    Add build logging

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ export PATH="/usr/lib/ccache/bin:$PATH"
 export CROSS_COMPILE="ccache /home/kailashrs/5z/arm64-gcc/bin/aarch64-elf-"
 export CROSS_COMPILE_ARM32="ccache /home/kailashrs/5z/arm32-gcc/bin/arm-eabi-"
 make O=out Z01R_defconfig
-make O=out -j$(nproc --all)
+make O=out -j$(nproc --all) 2>&1 | tee build.log
 cd ..
 cd AnyKernel3
 rm zImage
