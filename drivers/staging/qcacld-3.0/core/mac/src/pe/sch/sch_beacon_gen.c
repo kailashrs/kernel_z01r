16aae37b691e9b1fb7971ba61962182e1a6b2178
Kailash <kailash.sudhakar@gmail.com>
2020-05-26 22:58:53 +0530

    qcacld-3.0: Fix warnings

--- drivers/staging/qcacld-3.0/core/mac/src/pe/sch/sch_beacon_gen.c
+++ drivers/staging/qcacld-3.0/core/mac/src/pe/sch/sch_beacon_gen.c
@@ -1026,7 +1026,7 @@ void sch_process_pre_beacon_ind(tpAniSirGlobal pMac, tpSirMsgQ limMsg)
 					       psessionEntry);
 		else
 			pe_err("can not send beacon for PEER session entry");
-			break;
+		break;
 
 	case eLIM_AP_ROLE: {
 		uint8_t *ptr =
@@ -1044,7 +1044,7 @@ void sch_process_pre_beacon_ind(tpAniSirGlobal pMac, tpSirMsgQ limMsg)
 		} else
 			pe_err("can not send beacon for PEER session entry");
 			}
-			break;
+		break;
 
 	default:
 		pe_err("Error-PE has Receive PreBeconGenIndication when System is in %d role",
