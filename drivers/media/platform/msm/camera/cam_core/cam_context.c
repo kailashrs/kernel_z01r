d8ecd0b276f746ca095f4c112ee61fdb60b22ca6
Vishalsingh Hajeri <vhajeri@codeaurora.org>
2020-06-18 17:12:40 +0530

    msm: camera: context: Add null check on context pointer
    Check for context pointer being null, before accessing its
    members.
    
    Bug: 129852075
    Change-Id: I778c44e743edddcddf0f2fd6fd49336dfe00baf0
    Signed-off-by: Vishalsingh Hajeri <vhajeri@codeaurora.org>

--- drivers/media/platform/msm/camera/cam_core/cam_context.c
+++ drivers/media/platform/msm/camera/cam_core/cam_context.c
@@ -204,7 +204,7 @@ int cam_context_handle_crm_process_evt(struct cam_context *ctx,
 {
 	int rc = 0;
 
-	if (!ctx->state_machine) {
+	if (!ctx || !ctx->state_machine) {
 		CAM_ERR(CAM_CORE, "Context is not ready");
 		return -EINVAL;
 	}
@@ -298,7 +298,7 @@ int cam_context_handle_flush_dev(struct cam_context *ctx,
 {
 	int rc = 0;
 
-	if (!ctx->state_machine) {
+	if (!ctx || !ctx->state_machine) {
 		CAM_ERR(CAM_CORE, "Context is not ready");
 		return -EINVAL;
 	}
