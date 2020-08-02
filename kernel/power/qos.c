2fe0b5a79e2277da5c4de0af96899e3a10547208
Kailash <kailash.sudhakar@gmail.com>
2020-08-02 20:33:59 +0530

    Revert "pm_qos: Log _add_request calls"
    This reverts commit 43458bc401de161f44d3c2028da5fd0213913f81.

--- kernel/power/qos.c
+++ kernel/power/qos.c
@@ -575,8 +575,8 @@ static void pm_qos_irq_notify(struct irq_affinity_notify *notify,
  * removal.
  */
 
-void pm_qos_add_request_special(struct pm_qos_request *req,
-			int pm_qos_class, s32 value, const char* str)
+void pm_qos_add_request(struct pm_qos_request *req,
+			int pm_qos_class, s32 value)
 {
 	if (!req) /*guard against callers passing in null */
 		return;
@@ -585,8 +585,6 @@ void pm_qos_add_request_special(struct pm_qos_request *req,
 		WARN(1, KERN_ERR "pm_qos_add_request() called for already added request\n");
 		return;
 	}
-
-	pr_info("AAAA: pm_qos_add_request called by: %s\n", str);
 
 	switch (req->type) {
 	case PM_QOS_REQ_AFFINE_CORES:
@@ -652,7 +650,7 @@ void pm_qos_add_request_special(struct pm_qos_request *req,
 	}
 #endif
 }
-EXPORT_SYMBOL_GPL(pm_qos_add_request_special);
+EXPORT_SYMBOL_GPL(pm_qos_add_request);
 
 /**
  * pm_qos_update_request - modifies an existing qos request
