bde2f42981a854de8807bb1ccfbea013e8461e78
Kailash <kailash.sudhakar@gmail.com>
2020-09-23 18:52:00 +0530

    buildscript: LLVM Gang!

--- build.sh
+++ build.sh
@@ -1,24 +1,24 @@
-aarch64_tc="aarch64-linux-gnu-"
-arm_tc="arm-none-eabi-"
+workdir="$HOME/5z"
+srcdir="$HOME/5z/msm-4.9"
+objdir="$srcdir/out/arch/arm64/boot"
 
-export PATH="/usr/lib/ccache/bin:$PATH"
+aarch64_tc="aarch64-linux-gnu-"
+arm_tc="arm-linux-gnueabi-"
+
+export PATH="/usr/lib/ccache/bin:$workdir/proton-clang/bin:$PATH"
 
 export KBUILD_BUILD_USER="spac3b0y"
 export KBUILD_BUILD_HOST="TheHurtlocker"
 
 export ARCH="arm64"
 
-export CROSS_COMPILE="ccache $aarch64_tc"
-export CROSS_COMPILE_ARM32="ccache $arm_tc"
-
-workdir="$HOME/5z"
-srcdir="$HOME/5z/msm-4.9"
-objdir="$srcdir/out/arch/arm64/boot"
+export CROSS_COMPILE="$aarch64_tc"
+export CROSS_COMPILE_ARM32="$arm_tc"
 
 build() {
  make clean
  make O=out Z01R_defconfig
- make O=out -j$(nproc --all) 2>&1 | tee build.log
+ make CC="ccache clang" AR=llvm-ar AS=llvm-as NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip LD=ld.lld O=out -j$(nproc --all) 2>&1 | tee build.log
 }
 
 name_zip() {
